5130ada55e3341b83ad6bb9515ca47b51d2dc618
Robert Cabrera <robertcabrera4871@gmail.com>
2020-02-07 15:20:23 -0500

    added time since last clicked function to textView

--- app/src/main/java/com/example/inclasstextboxassign/MainActivity.kt
+++ app/src/main/java/com/example/inclasstextboxassign/MainActivity.kt
@@ -8,20 +8,28 @@ import android.view.View
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import java.sql.Time
+import java.time.Instant
+import java.time.LocalTime
+import java.util.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-
+    var startTime = Calendar.getInstance().timeInMillis
+    var timeSinceLast: Long = 0
+    var clickTime: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         myTextBox.setOnClickListener { view ->
-            var textNumber = (view as TextView).text.toString().toInt()
-            textNumber++
-            view.text = textNumber.toString()
+             clickTime = Calendar.getInstance().timeInMillis
+             timeSinceLast = (clickTime - startTime) / 1000
+            (view as TextView).text = timeSinceLast.toString().plus(getString(R.string.seconds_passed))
+            startTime = clickTime
         }
         var changeColor = fun (v: View?){
+
             (v as ConstraintLayout).setBackgroundColor(Color.rgb(Random.nextInt(),Random.nextInt(),Random.nextInt()))
         }
         myLayout.setOnLongClickListener {
